5f97877cb33781c0f826ef3dff6cfd38c9f48a80
Michel Mahadeva <michel.mahadeva@edu.tbz.ch>
2026-01-15 15:43:33 +0100

    add modify authority

--- frontend_react/src/config/Authorities.ts
+++ frontend_react/src/config/Authorities.ts
@@ -5,6 +5,7 @@ enum authorities {
   USER_CREATE = "USER_CREATE",
   USER_READ = "USER_READ",
   USER_DEACTIVATE = "USER_DEACTIVATE",
+  USER_MODIFY = "USER_MODIFY"
 }
 
 export default authorities;
