b80692b7cf512e22e4286cadd2aa7d1cceb9dff7
Michel Mahadeva <michel.mahadeva@edu.tbz.ch>
2026-01-16 10:41:09 +0100

    create admin page with switch to show users

--- frontend_react/src/components/pages/AdminListPage/AdminListTable.tsx
+++ frontend_react/src/components/pages/AdminListPage/AdminListTable.tsx
@@ -3,79 +3,173 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import { List, Importance } from "../../../types/models/List.model";
+import { User } from "../../../types/models/User.model";
 import ListService from "../../../Services/ListService";
+import UserService from "../../../Services/UserService";
 import { useNavigate } from "react-router-dom";
 import Link from "@mui/material/Link";
 
-
 const AdminListTable = () => {
   const navigate = useNavigate();
+  const [showList, setShowList] = useState<boolean>(
+    JSON.parse(
+      (localStorage.getItem("showList") as unknown as string).toLowerCase()
+    )
+  );
+  const [users, setUsers] = useState<User[]>([]);
   const [lists, setLists] = useState<List[]>([]);
 
+  const handleShowListChange = (newValue: boolean) => {
+    setShowList(newValue);
+    localStorage.setItem("showList", newValue as unknown as string);
+  };
+
   useEffect(() => {
+    UserService.getAllUsers().then((data) => {
+      setUsers(data.data);
+    });
     ListService.getAllListsAdmin().then((data) => {
       setLists(data);
     });
   }, []);
+  console.log("Users", users);
 
-  const handleAdd = () => {
-    navigate("../list/edit/admin");
-  };
+  if (showList) {
+    const handleAdd = () => {
+      navigate("../list/edit/admin");
+    };
 
-  const handleEdit = (id: string) => {
-    navigate("../list/edit/admin/" + id);
-  };
+    const handleEdit = (id: string) => {
+      navigate("../list/edit/admin/" + id);
+    };
 
-  const handleDelete = (id: string) => {
-    ListService.deleteList(id);
-    window.location.reload();
-  };
+    const handleDelete = (id: string) => {
+      ListService.deleteList(id);
+      window.location.reload();
+    };
+    return (
+      <>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={showList}
+              onChange={(e) => handleShowListChange(!showList)}
+            />
+          }
+          label="Show List Entries"
+        />
 
-  return (
-    <>
-          <Link href="/list">To the List</Link>
-      {lists.map((list) => (
-        <div key={list.id}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent sx={{ borderBottom: "1px solid" }}>
-              Author: {list.user.firstName} {list.user.lastName} <br />
-              Priority: {Importance[list.importance]} <br /> {list.title} <br />
-              -------------------------------------- <br />
-              {list.text}
-              <br /><br />
-              <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                variant="contained"
-                onClick={() => handleEdit(list.id)}
-              >
-                Edit
-              </Button>
-              <Button
-                size="small"
-                color="error"
-                variant="contained"
-                onClick={() => handleDelete(list.id)}
-              >
-                Delete
-              </Button>
-              </CardActions>
-            </CardContent>
-          </Card>
-        </div>
-      ))}
-      <Button
-        size="small"
-        color="success"
-        variant="contained"
-        onClick={handleAdd}
-      >
-        Add
-      </Button>
-    </>
-  );
+        <Link href="/list">To the List</Link>
+        {lists.map((list) => (
+          <div key={list.id}>
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent sx={{ borderBottom: "1px solid" }}>
+                Author: {list.user.firstName} {list.user.lastName} <br />
+                Priority: {Importance[list.importance]} <br /> {list.title}{" "}
+                <br />
+                -------------------------------------- <br />
+                {list.text}
+                <br />
+                <br />
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    variant="contained"
+                    onClick={() => handleEdit(list.id)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    size="small"
+                    color="error"
+                    variant="contained"
+                    onClick={() => handleDelete(list.id)}
+                  >
+                    Delete
+                  </Button>
+                </CardActions>
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+        <Button
+          size="small"
+          color="success"
+          variant="contained"
+          onClick={handleAdd}
+        >
+          Add
+        </Button>
+      </>
+    );
+  } else {
+    const handleAdd = () => {
+      navigate("../user/edit/");
+    };
+
+    const handleEdit = (id: string) => {
+      navigate("../user/edit/" + id);
+    };
+
+    const handleDelete = async (id: string) => {
+      await UserService.deleteUser(id);
+      window.location.reload();
+    };
+    return (
+      <>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={showList}
+              onChange={(e) => handleShowListChange(!showList)}
+            />
+          }
+          label="Show Users"
+        />
+        <Link href="/list">To the List</Link>
+        {users.map((user: User) => {
+          {console.log("User", user.firstName)}
+          <div key={user.id}>
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                {user.firstName} {user.lastName} {user.email}
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    variant="contained"
+                    onClick={() => handleEdit(user.id)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    size="small"
+                    color="error"
+                    variant="contained"
+                    onClick={() => handleDelete(user.id)}
+                  >
+                    Delete
+                  </Button>
+                </CardActions>
+              </CardContent>
+            </Card>
+          </div>
+        })}
+        <Button
+          size="small"
+          color="success"
+          variant="contained"
+          onClick={handleAdd}
+        >
+          Add
+        </Button>
+      </>
+    );
+  }
 };
 
 export default AdminListTable;
